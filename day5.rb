68254aa2367e51bec52c1ec32f4f9f9e8ba16b70
hannakebedom <hanna.kebedom@shopify.com>
2022-12-05 13:34:56 -0500

    day 5: part 2

--- day5.rb
+++ day5.rb
@@ -10,11 +10,9 @@ def populate(stacks, file, directions)
   for line in file do
     if line[0] != "m" && line[1] != "1"
       index = 0
-      puts line
       for i in (4..line.length+1).step(4) do
         box = line[i-4...i]
         stacks[index].unshift(box[1]) if box[1] != " "
-        puts stacks.inspect
         index += 1
       end
     elsif line[1] != "1"
@@ -24,32 +22,37 @@ def populate(stacks, file, directions)
   end
 end
 
-def move(stacks, directions)
+def move9000(stacks, directions)
   for direction in directions
     quantity, from, to = direction
     to -= 1
     from -=1
-    puts stacks.inspect
-    puts "Moving #{quantity} from #{from + 1} to #{to + 1}"
+
     for i in 1..quantity do
       stacks[to].push(stacks[from].pop)
     end
-    puts stacks.inspect
+  end
+end
+
+def move9001(stacks, directions)
+  for direction in directions
+    quantity, from, to = direction
+    to -= 1
+    from -=1
+    
+    stacks[to].push(*stacks[from].pop(quantity))
   end
 end
 
 populate(stacks, file, directions)
-move(stacks, directions)
-
-
-puts stacks.inspect
+move9001(stacks, directions)
 
 result = ""
 for stack in stacks do
   result << stack[-1]
 end
+
 puts result
-# def read_move_statement()
 
 
 
