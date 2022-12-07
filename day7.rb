456c7fffa1e708c39d6d0f3973a6f9fa441d6203
Hanna Kebedom <hanna.kebedom@shopify.com>
2022-12-07 16:20:27 -0500

    day 7 attempt

--- day7.rb
+++ day7.rb
@@ -2,34 +2,71 @@
 require_relative 'fileops'
 
 class Day7
-  attr_accessor :file, :file_system
+  attr_accessor :file, :file_system, :dir_sizes
   
   def initialize(file_name)
     @file = FileOps.read(file_name)
-    @file_system = { "/" => { build_file_system(@file[1..-1], ["/"], 0) } }
+    @file_system = {"/" => {}}
+    @dir_sizes = {}
+    read_commands
   end
     
-  def build_file_system(file, path, parent_level)
-    for i in file.length
-      curr_level = file[i].length - file[i].lstrip.length
-      continue if curr_level - parent_level > 2
+  def read_commands
+    current_dir = @file_system["/"]
+    prev_dir = [] # stack
     
-      dir = file[i].end_with?('(dir)')
-      name = file[i].scan(/- (.*) \(/)
-      path << name
-      
-      if dir
-        puts "dir name: #{name}"
-        @file_system.dig(path)&.[] = { build_file_system(@file[1..-1], path, curr_level) }
+    for line in @file do
+      if line.start_with?("$ cd")
+        target = line.split(' ')[-1]
+        if target == '/'
+          prev_dir = []
+          current_dir = @file_system["/"]
+        elsif target == '..'
+          current_dir = prev_dir[-1]
+          prev_dir.pop
+        else
+          prev_dir.push(current_dir)
+          current_dir = current_dir[target]
+        end
+      elsif line.start_with?("$ ls")
+        next
       else
-        puts "file name: #{name}" 
-        # get file size too!
-        @file_system.dig(path)&.[] = 0
+        type, name = line.split(' ')
+        if type == "dir"
+          current_dir[name] = { } if !current_dir.key?(name)
+        else
+          current_dir[name] = type.to_i if !current_dir.key?(name)
+        end
       end
     end
   end
   
+  
+  def size(hash)
+    size = 0
+    hash.each do |key, value|
+      if value.is_a?(Hash)
+        dir_size = size(value)
+        @dir_sizes[key] = dir_size
+        size += dir_size
+      else
+        size += value
+      end
+    end
+    @dir_sizes["/"] = size
+    return size
+  end
+  
+  def sum_directories
+    sum = 0
+    @dir_sizes.each do |key, value|
+      sum += value if value <= 100000
+    end
+    return sum
+  end
 end
 
-day7 = Day7.new('input/day7ex.txt')
+day7 = Day7.new('input/day7.txt')
+puts day7.size(day7.file_system["/"])
 puts day7.file_system
+puts day7.sum_directories
